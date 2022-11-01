29d36d799cf85372ab1f304949f37c471950d69a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-01 14:25:24 -0400

    Create tl_news.php

---
+++ system/modules/custom_clickthrough_button/languages/en/tl_news.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Custom Clickthrough Button
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/custom-clickthrough-button
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_news']['custom_clickthrough_legend'] = 'Brand Manager Settings';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_news']['clickthrough_copy']          = array('Custom Clickthrough Button', 'Enter the copy that this news items button will display');
