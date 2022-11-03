7e3afe3583468de67e280a37ac930d5a49e253cc
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-03 09:21:30 -0400

    Update tl_news.php

--- system/modules/custom_clickthrough_button/languages/en/tl_news.php
+++ system/modules/custom_clickthrough_button/languages/en/tl_news.php
@@ -12,6 +12,8 @@
 
 /* Legends */
 $GLOBALS['TL_LANG']['tl_news']['custom_clickthrough_legend'] = 'Custom Clickthrough Button';
+$GLOBALS['TL_LANG']['tl_news']['featured_video_legend'] = 'Featured Video';
 
 /* Fields */
 $GLOBALS['TL_LANG']['tl_news']['clickthrough_copy']          = array('Custom Clickthrough Button', 'Enter the copy that this news item\'s button will display');
+$GLOBALS['TL_LANG']['tl_news']['featured_video']          = array('Featured Video', 'Add a featured video that will replace the featured image on the full article page');
