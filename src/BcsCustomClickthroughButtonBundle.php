97984e3b29dc574848e9c73277657436cf4c1ef5
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-02-19 09:25:45 -0500

    Create BcsCustomClickthroughButtonBundle.php

---
+++ src/BcsCustomClickthroughButtonBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Glide
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/custom-clickthrough-button
+ */
+
+namespace Bcs\CustomClickthroughButtonBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsCustomClickthroughButtonBundle extends Bundle
+{
+}
