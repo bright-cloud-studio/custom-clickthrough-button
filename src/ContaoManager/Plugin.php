734965c8546d0a408621caaf00640d75863ee2ae
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-03-13 08:45:08 -0400

    Update Plugin.php

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -22,7 +22,7 @@ class Plugin implements BundlePluginInterface
     {
         return [
             BundleConfig::create('Bcs\CustomClickthroughButtonBundle\BcsCustomClickthroughButtonBundle')
-                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle']),
+                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle', 'Contao\NewsBundle\ContaoNewsBundle']),
         ];
     }
 }
