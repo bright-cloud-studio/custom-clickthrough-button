77d7d334546ec33b23789ee83786504adfb49830
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-03-13 08:34:00 -0400

    Update tl_news.php

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -1,12 +1,8 @@
 <?php
 
-declare(strict_types=1);
-
+use Contao\News;
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
-if(!isset($GLOBALS['TL_DCA']['tl_news'])) {
-    return;
-}
 
 //$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace("author", "author,clickthrough_copy", $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
 PaletteManipulator::create()
